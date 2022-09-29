86f142b382f3e2eb41ff2a65d433ce0de9dcc5d3
Sanjay <sunnymulchandani07@gmail.com>
2022-09-29 20:05:29 +0530

    mediaroot

--- main/urls.py
+++ main/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
@@ -25,3 +26,4 @@ urlpatterns = [
     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
+urlpatterns= urlpatterns+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
